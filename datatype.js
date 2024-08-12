8334e7e1791051f5d462553d07e65434c1c89dc7
Darshanrawat016 <darshanrawat016@gmail.com>
2024-08-12 13:51:22 -0700

    update

---
+++ datatype.js
@@ -0,0 +1,78 @@
+// Number
+
+let a = 12;
+
+console.log(typeof a)
+console.log(a)
+
+// String
+
+let a1 = '13';
+
+
+console.log(typeof a1)
+
+// String concatination
+
+let a2 = 14;
+let mind = "hello";
+
+let a3 = a2 + mind ;
+
+console.log(a3)
+console.log(typeof a3)
+
+// boolean
+
+let today = true;
+let tommorow = false;
+
+console.log(typeof today)
+console.log(typeof tommorow)
+
+// Undefined
+
+let mm;
+
+console.log(typeof mm)
+
+// Null
+
+let data = null;
+let data1;
+
+console.log(typeof data)
+console.log(typeof data1)
+
+// Object
+
+let  user = {Name: "Darshan",
+      age: 19,
+      study:"Enginerring"
+
+}  
+
+console.log(typeof user)
+
+// BigInt
+
+let a4 = 12345678901223240099220988899n;
+
+console.log(typeof a4)
+
+// Array
+
+let arry = [
+    1,
+    2.45,
+    "shan",
+    true,
+    { name: "Hello" },
+    7,
+    [1, 2, 4],
+    123n,
+    false,
+];
+
+console.log(arry)
+console.log(arry[3])
