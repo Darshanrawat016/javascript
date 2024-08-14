b5cb2db48d6237447dc5a96209f9658957432c48
Darshanrawat016 <darshanrawat016@gmail.com>
2024-08-13 18:57:55 -0700

    update

---
+++ operators.js
@@ -0,0 +1,55 @@
+// 45 % 6
+
+console.log(45%6)
+
+// 5645 % 10
+
+console.log( 5645 % 10)
+
+// 1234 / 10
+
+console.log( 1234 / 10)
+
+// 2**3
+console.log(2**3)
+
+// n1 n2  !=
+
+n1 = 10
+n2 = 13
+console.log(n1!= n2)
+
+// n1 == str
+
+let str = 10
+
+console.log(n1== str)
+
+// b1 === str2
+
+let b1 = "Darshan"
+let str2 = 34
+
+console.log(b1===str2)
+
+// logical 12<= && 12>3 && 12<3
+
+console.log(12 <= 12 > 3 && 12 < 3);
+
+// logical 12<3 || 12>3
+
+console.log(12<3 || 12>3);
+
+//  12<3
+
+console.log(12<3)
+
+// num
+
+let num = 34
+
+num = num+= 10
+
+console.log(num%=2)
+
+//
