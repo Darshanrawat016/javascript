b9a28721b1d3769d971883cadda974caf0cdbb39
Darshanrawat016 <darshanrawat016@gmail.com>
2024-08-20 21:18:34 -0700

    update

---
+++ conditions.js
@@ -0,0 +1,6 @@
+
+// check if number is negetive of positive
+// check if number is divisible by 2
+// check if year is leap year or not
+// check greatest of 2 number  // take 2 numbers and check greatest
+
